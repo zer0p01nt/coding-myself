35b1c54f379b4c654c1a1b9aa91a73d488e4973b
zer0p01nt <1027.bmy@gmail.com>
2025-11-02 02:58:05 +0900

    ✨ Feat: 연관 게시글 추천 기능 구현

--- src/components/read/RecommendationArea.tsx
+++ src/components/read/RecommendationArea.tsx
@@ -1,15 +1,25 @@
+import { useParams } from "react-router";
 import RecommendationItem from "./RecommendationItem";
+import { useAxios } from "../../hooks/useAxios";
 
 export default function RecommendationArea() {
+  const params = useParams();
+  const { data, isLoading, error } = useAxios<Post[]>(
+    `/posts/${params.id}/related`,
+    []
+  );
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error : {error}</div>;
+  if (data.length === 0) return null;
+
   return (
     <article className='page__recommend w-full max-w-200 my-4 mx-0'>
       <h3 className='page__recommend-title font-bold text-[1.8rem] mb-5 min-[420px]:text-2xl'>
         Recommend Reading
       </h3>
       <ul className='page__recommend-lists page__recommend-lists'>
-        {/* RecommendationItem */}
-        <RecommendationItem />
-        <RecommendationItem />
+        {data &&
+          data.map((post) => <RecommendationItem key={post.id} {...post} />)}
       </ul>
     </article>
   );
--- src/components/read/RecommendationItem.tsx
+++ src/components/read/RecommendationItem.tsx
@@ -1,29 +1,26 @@
 import { NavLink } from "react-router";
-import { dummyImage1 } from "../../assets/images/images";
 
-export default function RecommendationItem() {
+export default function RecommendationItem({
+  id,
+  title,
+  desc,
+  thumbnail,
+}: Post) {
   return (
     <li className='my-5 mx-0'>
-      <NavLink to=''>
+      <NavLink to={`/read/${id}`}>
         <div className='page__recommend-list flex flex-col gap-4 sm:flex-row'>
           <img
-            src={dummyImage1}
-            alt=''
+            src={thumbnail}
+            alt={title}
             className='page__recommend-img max-w-full rounded-md sm:max-w-[250px]'
           />
           <div>
             <h4 className='page__recommend-subtitle mb-2 text-[1.325rem]/7 font-bold min-[420px]:text-[1.175rem]'>
-              Why you don’t need more than 3 pieces of clothing
+              {title}
             </h4>
             <p className='page__recommend-desc text-base/6 text-[#4b4b4b] overflow-hidden line-clamp-4 min-[420px]:text-base'>
-              Et vitae, mollis euismod lobortis blandit amet sed amet. Amet ut
-              amet nisl tortor arcu non id nulla mauris neque nisl magna.Et
-              vitae, mollis euismod lobortis blandit amet sed amet. Amet ut amet
-              nisl tortor arcu non id nulla mauris neque nisl magna.Et vitae,
-              mollis euismod lobortis blandit amet sed amet. Amet ut amet nisl
-              tortor arcu non id nulla mauris neque nisl magna.Et vitae, mollis
-              euismod lobortis blandit amet sed amet. Amet ut amet nisl tortor
-              arcu non id nulla mauris neque nisl magna.
+              {desc}
             </p>
           </div>
         </div>
