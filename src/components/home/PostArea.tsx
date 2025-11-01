72456e8ab8046ddd1fc54e7f342b67957f56e443
zer0p01nt <1027.bmy@gmail.com>
2025-11-02 03:01:45 +0900

    ✨ Feat: 검색 기능 구현

--- src/components/home/PostArea.tsx
+++ src/components/home/PostArea.tsx
@@ -1,8 +1,15 @@
+import { useSearchParams } from "react-router";
 import { useAxios } from "../../hooks/useAxios";
 import PostItem from "./PostItem";
 
 export default function PostArea() {
-  const { data, error, isLoading } = useAxios<Post[]>("/posts", []);
+  const [searchParams] = useSearchParams();
+  const q = searchParams.get("q");
+  const { data, error, isLoading } = useAxios<Post[]>(
+    q ? "/posts/search?title=" + q : "/posts",
+    []
+  );
+
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error : {error}</div>;
   return (
--- src/components/home/SearchArea.tsx
+++ src/components/home/SearchArea.tsx
@@ -1,22 +1,47 @@
+import { useNavigate, useSearchParams } from "react-router";
 import { search } from "../../assets/images/images";
+import { useEffect, useRef, useState } from "react";
 
 export default function SearchArea() {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const [query, setQuery] = useState(() => searchParams.get("q") || "");
+  const debounceTimer = useRef<null | number>(null);
+  const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  useEffect(() => {
+    if (debounceTimer.current) {
+      clearTimeout(debounceTimer.current); // 이전 타이머 취소
+    }
+    debounceTimer.current = setTimeout(() => {
+      navigate(query ? "?q=" + query : "/");
+    }, 300);
+  }, [navigate, query]);
+
   return (
     <section className='search-area mb-16'>
       <article className='search-area__search font-search text-center'>
         <h2 className='search-area__title text-[2rem] font-bold mb-2 lg:text-4xl'>
-          The Sucoding Blog
+          The MY Blog
         </h2>
         <p className='search-area__description text-[1.15rem] text-primary mb-4 min-[320px]:text-lg lg:text-[1.35rem]'>
           A Blog About Food, Experience, and Recipes.
         </p>
-        <form method='get' className='search-area__form relative'>
+        <form
+          method='get'
+          className='search-area__form relative'
+          onSubmit={(e) => e.preventDefault()}
+        >
           <input
             type='text'
             name='q'
             placeholder='Search'
             className='search-area__input w-full py-2 px-4 border border-primary rounded-sm h-11.25'
             autoComplete='off'
+            value={query}
+            onChange={handleChange}
           />
           <button
             type='submit'
